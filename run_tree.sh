53f9701b0149f7176f1245e9c4d4af58982d67b6
Allie Lahnala <alahnala@cronus.cs.stonybrook.edu>
2024-09-09 15:32:14 -0400

    Automatic repository sync @ cronus

--- run_tree.sh
+++ run_tree.sh
@@ -1,5 +1,9 @@
-python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx_zephyr-7b-sft-full.txt -m diversity > data/results/TREE_preds_dlrxxx_zephyr-7b-sft-full.txt;
-python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx_zephyr-7b-sft-full122.txt -m diversity > data/results/TREE_preds_dlrxxx_zephyr-7b-sft-full122.txt;
+# python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx_zephyr-7b-sft-full.txt -m diversity > data/results/TREE_preds_dlrxxx_zephyr-7b-sft-full.txt;
+# python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx_zephyr-7b-sft-full122.txt -m diversity > data/results/TREE_preds_dlrxxx_zephyr-7b-sft-full122.txt;
 
 
-python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx_zephyr-7b-sft-full122.txt -m diversity -hu > data/results/TREE_human.txt;
+# python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx_zephyr-7b-sft-full122.txt -m diversity -hu > data/results/TREE_human.txt;
+
+# nice python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlr1e6_zephyr-7b-sft-full122_d230.txt -m diversity > data/results/TREE_preds_dlr1e6_zephyr-7b-sft-full122_d230.txt;
+# nice python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlr1e6_zephyr-7b-sft-full122_d231.txt -m diversity > data/results/TREE_preds_dlr1e6_zephyr-7b-sft-full122_d231.txt;
+nice python src/emp_metrics/run_empathy_eval.py -f data/empo/preds_dlrxxx30x_zephyr-7b-sft-full.txt -m diversity > data/results/TREE_preds_dlrxxx30x_zephyr-7b-sft-full.txt;
